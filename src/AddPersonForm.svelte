2f66647dde4c6d0ae3b85565446a57b92cf55724
Lucas <joselucasrs@gmail.com>
2022-05-10 23:22:12 -0300

    Forms with group bind.

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,13 @@
   let name: string;
   let beltColor: string;
   let age: number;
+  // let fighting = false;
+  // let sneaking = false;
+  // let running = false;
+  let skills: string[] = [];
 
   const handdleSubmit = (): void => {
-    console.log(name, beltColor, age);
+    console.log(name, beltColor, age, skills);
   };
 </script>
 
@@ -12,6 +16,15 @@
   <input type="text" placeholder="name" bind:value={name} />
   <input type="text" placeholder="belt color" bind:value={beltColor} />
   <input type="number" placeholder="age" bind:value={age} />
+  <label>Skills:</label>
+  <!-- <input type="checkbox" bind:checked={fighting} /> Fighting <br />
+  <input type="checkbox" bind:checked={sneaking} /> Sneaking <br />
+  <input type="checkbox" bind:checked={running} /> Running <br /> -->
+
+  <input type="checkbox" bind:group={skills} value="fighting" /> Fighting <br />
+  <input type="checkbox" bind:group={skills} value="sneaking" /> Sneaking <br />
+  <input type="checkbox" bind:group={skills} value="running" /> Running <br />
+
   <button type="submit">Add Person</button>
 </form>
 
