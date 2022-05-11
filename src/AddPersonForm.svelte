2dc6556c2637dfb0333e4671ab6e9014e5b3a5fa
Lucas <joselucasrs@gmail.com>
2022-05-10 23:30:26 -0300

    Form with select.

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,6 @@
   let name: string;
   let beltColor: string;
   let age: number;
-  // let fighting = false;
-  // let sneaking = false;
-  // let running = false;
   let skills: string[] = [];
 
   const handdleSubmit = (): void => {
@@ -14,16 +11,29 @@
 
 <form on:submit|preventDefault={handdleSubmit}>
   <input type="text" placeholder="name" bind:value={name} />
-  <input type="text" placeholder="belt color" bind:value={beltColor} />
   <input type="number" placeholder="age" bind:value={age} />
-  <label>Skills:</label>
-  <!-- <input type="checkbox" bind:checked={fighting} /> Fighting <br />
-  <input type="checkbox" bind:checked={sneaking} /> Sneaking <br />
-  <input type="checkbox" bind:checked={running} /> Running <br /> -->
 
-  <input type="checkbox" bind:group={skills} value="fighting" /> Fighting <br />
-  <input type="checkbox" bind:group={skills} value="sneaking" /> Sneaking <br />
-  <input type="checkbox" bind:group={skills} value="running" /> Running <br />
+  <strong>Skills:</strong>
+  <label>
+    <input type="checkbox" bind:group={skills} value="fighting" /> Fighting
+  </label>
+  <label>
+    <input type="checkbox" bind:group={skills} value="sneaking" /> Sneaking
+  </label>
+  <label>
+    <input type="checkbox" bind:group={skills} value="running" /> Running
+  </label>
+
+  <strong>Belt Color</strong>
+  <br />
+  <select bind:value={beltColor}>
+    <option value="black">black</option>
+    <option value="orange">orange</option>
+    <option value="brown">brown</option>
+    <option value="white">white</option>
+  </select>
+
+  <br />
 
   <button type="submit">Add Person</button>
 </form>
